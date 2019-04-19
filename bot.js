fdc0849a3dfccf427a72d490605baee216d502c0
medo67 <45091642+medo67@users.noreply.github.com>
2019-04-19 20:19:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["497364165384470529"]
+const developers = ["363021463739170816"]
 const adminprefix = "-";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
